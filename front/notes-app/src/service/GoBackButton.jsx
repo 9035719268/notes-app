15a76619dd05caea4eabe2f080c14c1b943cff7b
Alexander <9035719268a@gmail.com>
2021-11-03 20:31:48 +0300

    - Add go-back button;

---
+++ front/notes-app/src/service/GoBackButton.jsx
@@ -0,0 +1,11 @@
+import "./go-back-button.css";
+
+const GoBackButton = () => {
+  return (
+    <a href="/">
+      <button className="go-back-button">Вернуться в главное меню</button>
+    </a>
+  );
+};
+
+export default GoBackButton;
