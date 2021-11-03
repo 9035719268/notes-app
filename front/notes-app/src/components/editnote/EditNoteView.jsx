916ec816306491ac1d295394b3b8d4eaff1388c1
Alexander <9035719268a@gmail.com>
2021-11-03 21:26:11 +0300

    - Add edit note page;

--- front/notes-app/src/components/editnote/EditNoteView.jsx
+++ front/notes-app/src/components/editnote/EditNoteView.jsx
@@ -1,6 +1,51 @@
+import FetchNotes from "../fetchnotes/FetchNotes";
+import "./edit-note.css";
+import {editNote} from "../../service/requests";
+
 const EditNoteView = () => {
+  const handleSubmit = event => {
+    event.preventDefault();
+
+    const noteId = event.target[0].value;
+    const noteText = event.target[1].value;
+    const noteName = event.target[2].value;
+    const noteHashtags = event.target[3].value;
+
+    editNote(noteId, noteText, noteName, noteHashtags)
+      .then(() => window.location.reload());
+  };
+
   return (
-    <h1>EditNoteView</h1>
+    <div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label>
+            Введите ID заметки для редактирования:
+            <input type="text" placeholder="ID заметки" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Введите новый текст заметки:
+            <input type="text" placeholder="Текст заметки" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Введите новое название заметки (опционально):
+            <input id="edit-note-name" type="text" placeholder="Название заметки" />
+          </label>
+        </div>
+        <div>
+          <label>
+            Введите новые хэштеги для заметки (опционально):
+            <input id="edit-note-name" type="text" placeholder="Хэштеги для заметки" />
+          </label>
+        </div>
+        <button type="submit">Подтвердить</button>
+      </form>
+      <FetchNotes />
+    </div>
   );
 };
 
--- front/notes-app/src/service/requests.js
+++ front/notes-app/src/service/requests.js
@@ -4,7 +4,16 @@ export const addNote = () => axios.post("http://localhost:8080/note/add");
 
 export const deleteNote = noteId => axios.delete(`http://localhost:8080/note/${noteId}/delete`);
 
-export const editNote = noteId => axios.patch(`http://localhost:8080/note/${noteId}/edit`);
+export const editNote = (noteId, noteText, noteName, noteHashtags) => {
+  const hashtags = noteHashtags.split(", ").map(hashtag => (
+    {id: Math.random() * Date.now(), text: hashtag}
+  ));
+
+  return axios.patch(
+    `http://localhost:8080/note/${noteId}/edit`,
+    {id: noteId, text: noteText, name: noteName, hashtags: hashtags}
+  );
+};
 
 export const fetchNotes = () => axios.get("http://localhost:8080/notes");
 
